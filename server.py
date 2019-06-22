c19f6e4f01d40c88fbccc4c93b2eed7fd5ef2b2b
stevennyman <35897452+stevennyman@users.noreply.github.com>
2019-06-22 01:21:56 -0400

    Update server.py

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/', defaults={'dummy': ''})
+@app.route('/', defaults={'dummy': ''}) # URL catching based on: http://flask.pocoo.org/snippets/57/ and https://stackoverflow.com/a/14023930
 @app.route('/<path:dummy>')
 def hello_world(dummy):
     return 'Hello, World!'
